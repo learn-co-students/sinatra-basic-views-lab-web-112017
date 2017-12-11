17e1a482c902bf2870aefce791f1a1b4a47cb263
eal6gf <elum1234@gmail.com>
2017-12-11 13:20:44 -0500

    Done.

---
+++ models/BowlingAlley.rb
@@ -0,0 +1,10 @@
+class BowlingAlley
+  attr_accessor :name, :tagline, :address
+
+  def initialize(name, tagline, address)
+    @name = name
+    @tagline = tagline
+    @address = address
+  end
+  
+end
